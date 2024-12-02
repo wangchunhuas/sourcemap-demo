866da306246ca260a7790743f62a5d1e0115a0f3
wangchunhua <1509279565@qq.com>
2024-12-02 18:21:40 +0800

    auto:部署demo

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,12 @@
   <div>性能测试</div>
 </template>
 <script setup>
+import { onMounted } from "vue";
+onMounted(() => {
+  const num = null;
+  if (num.length > 1) {
+    console.log("发生了错误");
+  }
+});
 </script>
 <style scoped></style>
