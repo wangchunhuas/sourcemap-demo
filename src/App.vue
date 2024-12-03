37a1c76974e77666a3fb86d73ef2be8ab08108a4
wangchunhua <1509279565@qq.com>
2024-12-03 16:16:42 +0800

    sourcemap定位错误代码

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>性能测试</div>
+  <div class="container">
+    <div class="content">
+      <div><router-link to="/">Home</router-link></div>
+      <div><router-link to="/about">About</router-link></div>
+      <div><router-link to="/error">trigger</router-link></div>
+    </div>
+    <div class="viewcontent">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 <script setup>
 import { onMounted } from "vue";
@@ -10,4 +19,26 @@ onMounted(() => {
   }
 });
 </script>
-<style scoped></style>
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+.content {
+  width: 100%;
+  height: 20%;
+  background-color: #ccc;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.viewcontent {
+  flex: 1;
+  height: 100%;
+}
+.content div {
+  margin: 0 20px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import router from "./router/index";
 import ErrorStackParser from "error-stack-parser";
 import sourceMapReflectErrorCode from "./utils/index";
 const app = createApp(App);
+app.use(router);
+app.use(ElementPlus);
 app.config.errorHandler = (err, vm) => {
   const errorStack = ErrorStackParser.parse(err);
-  sourceMapReflectErrorCode(errorStack[0]);
+  // sourceMapReflectErrorCode(errorStack[0]);
+  const jsError = {
+    stack_frames: errorStack,
+    message: err.message,
+    stack: err.stack,
+    error_name: err.name,
+  };
+  vm.$message.error(`您触发了一个${err.name} 错误`);
+  localStorage.setItem("jserror", JSON.stringify(jsError));
 };
 app.mount("#app");
