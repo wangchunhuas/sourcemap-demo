ffe7f2288de58eeba38d9ad5e91fbab5f94a584b
wangchunhuas <wangchunhuas@users.noreply.github.com>
2024-12-05 15:46:19 +0000

    deploy: 1c374020e70f1766b3d6793da486d8982759c0b9

--- assets/About-mhZotv2i.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,c,o}from"./index-Ig13AOvT.js";const t={};function n(r,s){return o(),c("div",null,"关于页面")}const a=e(t,[["render",n]]);export{a as default};
-//# sourceMappingURL=About-mhZotv2i.js.map
---
+++ assets/events-DAyUNvSG.js
@@ -0,0 +1,2 @@
+import{f as t}from"./index-BN_SWOU0.js";const n=t("events",{state:()=>({events:[]}),getters:{},actions:{setEvents(e){this.events=e},getEvents(){return this.events}}});export{n as u};
+//# sourceMappingURL=events-DAyUNvSG.js.map
